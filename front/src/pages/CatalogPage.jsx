ded1c15df753d8f65adba6f613fef7d572e6568a
Abbos29 <abbosvaliev2003@gmail.com>
2024-07-29 18:47:29 +0500

    feat: last changes

--- front/src/pages/CatalogPage.jsx
+++ front/src/pages/CatalogPage.jsx
@@ -1,9 +1,11 @@
 import Catalog from "../components/layout/Catalog/Catalog"
+import Filter from "../components/layout/Filter/Filter"
 
 
 const CatalogPage = () => {
   return (
     <>
+      <Filter />
       <Catalog />
     </>
   )
